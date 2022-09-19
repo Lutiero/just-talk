b4aa8a477a1c036fcb5759a33b83f652aea4411b
keizo <13keizo@gmail.com>
2022-09-19 18:10:56 -0400

    ready

--- front/src/lib/components/Groups/Chat.svelte
+++ front/src/lib/components/Groups/Chat.svelte
@@ -1,89 +1,91 @@
 <script></script>
+    <div id="comments">
+        <div class="central">
     
-    <div class="central">
-    
-        <div class="server">
-            <div class="servidor">
-                <img src="../public/images/elijah-hiett-umfpFoKxIVg-unsplash.png" alt="">
-                <label for="">Alison Andrade</label>
-                <b><a href="/" class="resposta">3 respostas ></a></b>   
+            <div class="server">
+                <div class="servidor">
+                    <img src="../public/images/elijah-hiett-umfpFoKxIVg-unsplash.png" alt="">
+                    <label for="">Alison Andrade</label>
+                    <b><a href="/" class="resposta">3 respostas ></a></b>   
+                </div>
+                <label for="">Lorem ipsum dolor sit amet consectetur adipisicing elit.</label>
             </div>
-            <label for="">Lorem ipsum dolor sit amet consectetur adipisicing elit.</label>
-        </div>
+            
+            <div class="user">
         
-    
-    
+                <div class="usuario">
+                    <label for="" class="username">Você</label>
+                    <img src="../public/images/nick-arnot-AuCrXk0pLWo-unsplash.png" alt=""><br>
+                </div>
+                <label for="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit minima voluptatem exercitationem doloribus necessitatibus, magnam ut ullam tempora dolores incidunt, harum ipsa.</label>
+            </div>
+        
+            <!-- Somente para Separar -->
+        
+            <div class="server">
+                <div class="servidor">
+                    <img src="../public/images/elijah-hiett-umfpFoKxIVg-unsplash.png" alt="">
+                    <label for="">Alison Andrade</label>
+                    <b><a href="/" class="resposta">16 respostas ></a></b>   
+                </div>
+                <label for="">Lorem ipsum dolor sit amet consectetur adipisicing elit.</label>
+            </div>
+        
+        
+        
         <div class="user">
-    
+        
             <div class="usuario">
-                <label for="" class="username">Você</label>
+                <label for="">Você</label>
                 <img src="../public/images/nick-arnot-AuCrXk0pLWo-unsplash.png" alt=""><br>
             </div>
             <label for="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit minima voluptatem exercitationem doloribus necessitatibus, magnam ut ullam tempora dolores incidunt, harum ipsa.</label>
         </div>
-    
+        
+        
+        
+        
         <!-- Somente para Separar -->
-    
+        
         <div class="server">
             <div class="servidor">
                 <img src="../public/images/elijah-hiett-umfpFoKxIVg-unsplash.png" alt="">
-                <label for="">Alison Andrade</label>
-                <b><a href="/" class="resposta">16 respostas ></a></b>   
+                <label for="">Alison Andrade</label>   
             </div>
             <label for="">Lorem ipsum dolor sit amet consectetur adipisicing elit.</label>
         </div>
-    
-    
-    
-    <div class="user">
-    
-        <div class="usuario">
-            <label for="">Você</label>
-            <img src="../public/images/nick-arnot-AuCrXk0pLWo-unsplash.png" alt=""><br>
+        
+        
+        <div class="user">
+        
+            <div class="usuario">
+                <h5 for="">Você</h5>
+                <img src="../public/images/nick-arnot-AuCrXk0pLWo-unsplash.png" alt=""><br>
+            </div>
+            <label for="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit minima voluptatem exercitationem doloribus necessitatibus, magnam ut ullam tempora dolores incidunt, harum ipsa.</label>
         </div>
-        <label for="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit minima voluptatem exercitationem doloribus necessitatibus, magnam ut ullam tempora dolores incidunt, harum ipsa.</label>
-    </div>
-    
-    
-    
-    
-    <!-- Somente para Separar -->
-    
-    <div class="server">
-        <div class="servidor">
-            <img src="../public/images/elijah-hiett-umfpFoKxIVg-unsplash.png" alt="">
-            <label for="">Alison Andrade</label>   
+        <!-- Somente para Separar -->
+            <div class="input">
+                <form>
+                    <img src="../public/images/nick-arnot-AuCrXk0pLWo-unsplash.png" alt="">
+                    <input type="text" class="mensage" placeholder="Escrever mensagem...">
+                </form>
+            </div>
         </div>
-        <label for="">Lorem ipsum dolor sit amet consectetur adipisicing elit.</label>
     </div>
-    
-    
-    <div class="user">
-    
-        <div class="usuario">
-            <label for="">Você</label>
-            <img src="../public/images/nick-arnot-AuCrXk0pLWo-unsplash.png" alt=""><br>
-        </div>
-        <label for="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit minima voluptatem exercitationem doloribus necessitatibus, magnam ut ullam tempora dolores incidunt, harum ipsa.</label>
-    </div>
-    
-    
-    <!-- Somente para Separar -->
-    
-    
-    
-        <div class="texto">
-            <form>
-                <img src="../public/images/nick-arnot-AuCrXk0pLWo-unsplash.png" alt="">
-                <input type="text" class="mensage">
-                <input type="button" value="Enviar" class="botao">
-            </form>
-    
-        </div>
-    
-    </div>
+   
     
     <style>
+        #comments{
+            background-color: #FAFBFC;
+            padding-top: 1px;
+        }
+        #comments label {
+            color: #343342;
+            font-size: 13px;
+            letter-spacing: 0.26px;
+            font-family: Gilroy;
+        }
         .user{
             text-align: right;
             margin-top: 2rem;
@@ -91,10 +93,25 @@
         .mensage{
             text-align: left;
         }
-        .texto{
+        .input{
             margin-top: 2rem;   
         }
-        
+        #comments input {
+            width: 100%;
+            padding-top: 10px;
+            padding-bottom: 10px;
+            background: #FFFFFF;
+        }
+        #comments ::placeholder{
+            padding-left: 40px;
+            color: #A7B6C4;
+        }
+        input {
+            outline: 0;
+            border-width: 0 0 2px;
+            border-color: white;
+        }
+
         .server{
             margin-top: 2rem;
         }
