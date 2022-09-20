52c5129e67d58747f0958dbe0466adc7a20b7e92
Lutiero Pedroso Fabri <lutiero@gmail.com>
2022-09-20 19:09:57 -0400

    teste com o japônes

--- front/src/routes/themes/[slug]/+page.js
+++ front/src/routes/themes/[slug]/+page.js
@@ -3,7 +3,7 @@ import { error } from '@sveltejs/kit';
 
 /** @type {import('../../../../.svelte-kit/types/src/routes/themes/$types').PageLoad} */
 export const load = async () => {
-	let response = await fetch(`http://localhost:3000/themes${params.slug}`);     
+	let response = await fetch(`http://localhost:3000/themes${params.slug}`)    
     response = await response.json();
     console.log("Response", response)
     return {
