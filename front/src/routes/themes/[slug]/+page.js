c1a745d8d8f1cfc12539e2792dc6915b5d65cb9b
Lutiero Pedroso Fabri <lutiero@gmail.com>
2022-09-20 14:15:27 -0400

    ajuste de rota do front

--- front/src/routes/themes/[slug]/+page.js
+++ front/src/routes/themes/[slug]/+page.js
@@ -1,7 +1,7 @@
 
 import { error } from '@sveltejs/kit';
 
-/** @type {import('./$types').PageLoad} */
+/** @type {import('../../../../.svelte-kit/types/src/routes/themes/$types').PageLoad} */
 export const load = async () => {
 	let response = await fetch(`http://localhost:3000/themes${params.slug}`);     
     response = await response.json();
