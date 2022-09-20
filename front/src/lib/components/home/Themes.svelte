0a526ac216e10893b22eee37c7bc725558848117
Lutiero Pedroso Fabri <lutiero@gmail.com>
2022-09-20 13:43:39 -0400

    ajuste de link da rota

--- front/src/lib/components/home/Themes.svelte
+++ front/src/lib/components/home/Themes.svelte
@@ -14,7 +14,7 @@
 		{#each themes as theme}
 			<li>
 				<div class="image">
-					<a href="/{theme.id}">
+					<a href="theme/{theme.id}">
 						<img src={theme.thumbnail} alt={theme.title} />
 					</a>
 					<span class="image-title"
