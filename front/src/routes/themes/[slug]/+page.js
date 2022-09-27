c0503c2ce5630630fcc5788022fb9d9da660b5f8
Lutiero Pedroso Fabri <lutiero@gmail.com>
2022-09-26 20:49:04 -0400

    professor ajustou promisses

--- front/src/routes/themes/[slug]/+page.js
+++ front/src/routes/themes/[slug]/+page.js
@@ -1,36 +1,35 @@
 /** @type {import('./$types').PageLoad} */
 export async function load({params}) {
-    const myThemesRequest = await fetch(`http://localhost:3000/themes/${params.slug}`,{
+    const myThemesRequest = fetch(`http://localhost:3000/themes/${params.slug}`,{
         method: 'GET',
         headers: {
             'ContentType': 'application/json'
         }
     });
 
-    const themeResponse = await myThemesRequest.json();
-
-    const myTopicRequest = await fetch(`http://localhost:3000/themes/${params.slug}/topics`, {
+    
+    const myTopicRequest = fetch(`http://localhost:3000/themes/${params.slug}/topics`, {
       method: 'GET',
       headers: {
         'ContentType': 'application/json'
       }
     });
-
-    const topicResponse = await myTopicRequest.json();
-
-    const myRepliesRequest = await fetch(`http://localhost:3000/themes/${params.slug}/topics/replies`, {
+    
+    
+    const myRepliesRequest = fetch(`http://localhost:3000/themes/${params.slug}/topics/replies`, {
       method: 'GET',
       headers: {
         'ContentType': 'application/json'
       }
     });
-
-    const repliesResponse = await myRepliesRequest.json();
+    
+    const requests = await Promise.all([myThemesRequest, myTopicRequest, myRepliesRequest])
+    const responses = await Promise.all([requests[0].json(), requests[1].json(), requests[2].json()]);
     
     return {
-      theme: themeResponse,
-      topics: topicResponse,
-      replies: repliesResponse
+      theme: responses[0],
+      topics: responses[1],
+      replies: responses[2]
     };
   }
 
