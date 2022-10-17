1d50a7e7ffd394cc4ee8386a40cad75475a7ffa1
keizo <13keizo@gmail.com>
2022-10-16 20:44:32 -0400

    starting

---
+++ front/src/routes/begin/begin.svelte
@@ -0,0 +1,24 @@
+
+<div class="begin">
+  <h1>Just Talk</h1>
+  <p>Não desafie seus problemas sozinho(a), converse com pessoas ou profissionais que já passaram por algo parecido.</p>
+</div>
+
+<style>
+  .begin{
+    background: transparent url('icons/begin.svg') 0% 0% no-repeat padding-box;
+  }
+  .begin h1{
+    font: normal normal 800 57px/82px Gilroy;
+    letter-spacing: 2.85px;
+    color: #FAFBFC;
+    opacity: 0.65;
+  }
+  .begin p{
+    font: normal normal normal 16px/22px Gilroy;
+    letter-spacing: 0px;
+    color: #FAFBFC;
+    opacity: 1;
+  }
+
+</style>
